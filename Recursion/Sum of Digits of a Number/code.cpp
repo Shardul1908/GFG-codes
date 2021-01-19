40472b8d6f949dc1592876598e2d69813ad30c01
Shardul Dharmadhikari <sharduld80@gmail.com>
2021-01-19 12:04:21 +0530

    Create code.cpp

---
+++ Recursion/Sum of Digits of a Number/code.cpp
@@ -0,0 +1,45 @@
+//Initial Template for C++
+
+
+
+#include <iostream>
+using namespace std;
+
+
+
+ // } Driver Code Ends
+
+
+//User function Template for C++
+
+// Complete this function
+int sumOfDigits(int n)
+{
+    if(n > 0) {
+        return (n%10 + sumOfDigits(n/10));
+    }
+    return 0;
+}
+
+// { Driver Code Starts.
+
+
+int main() {
+	int T;
+	
+	//taking testcases
+	cin>>T;
+	while(T--)
+	{
+	    int n;
+	    
+	    //taking input n
+	    cin>>n;
+	    
+	    //calling method sumOfDigits()
+	    cout<<sumOfDigits(n)<<endl;
+	    
+	    
+	}
+	return 0;
+}  // } Driver Code Ends
